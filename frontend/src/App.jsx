9a16a624ce31d47c6f23e54f498eaa88f6e0b372
saloni singh <saloni.singh301295@gmail.com>
2025-08-24 11:08:16 +0530

    initial start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h3 className='text-primary-emphasis bg-primary-subtle'>it working</h3>
+    </>
+  )
+}
+
+export default App
